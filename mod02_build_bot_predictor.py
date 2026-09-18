5fa6f9c04743d86faf621a173720ce7b4fe6672e
Quinn <frank.q.gu@gmail.com>
2026-09-17 23:47:15 -0400

    s

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -12,8 +12,8 @@ def train_model(X, y, seed=seed):
     model = GradientBoostingClassifier(
         learning_rate=0.1,
         n_estimators=100,
-        max_depth=8,
-        subsample=1,
+        max_depth=3,        
+        subsample=0.8,       
         min_samples_leaf=1,
         random_state=seed
     )
